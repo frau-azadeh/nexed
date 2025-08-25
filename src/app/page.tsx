7fc5902667c94bae1349dffeac0b1b38de005e22
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-25 15:30:53 +0330

    update and test font and tailwind color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-primary">مدرسه دختران ایران زمین</h1>
+      <h1 className="text-primary text-center">مدرسه دختران ایران زمین</h1>
     </div>
   );
 }
