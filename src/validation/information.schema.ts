2c1bb6d5c6cd91267d3fdc14800d62e8705c4f22
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-27 21:03:34 +0330

    add information schema for form

---
+++ src/validation/information.schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const InformationSchema = z.object({
+  firstName: z
+    .string({ required_error: "نام الزامی است" })
+    .min(2, "نام باید حداقل 2 کاراکتر باشد"),
+  lastName: z
+    .string({ required_error: "نام خانوادگی الزامی است" })
+    .min(2, "نام خانوادگی باید حداقل 2 کاراکتر باشد"),
+  maeitalStatus: z.enum(["single", "married", "divorces"]),
+  fatherName: z.string().min(2, "نام پدر باید حداقل 2 کاراکتر باشد").optional(),
+});
+
+export type informationFormValue = z.infer<typeof InformationSchema>;
