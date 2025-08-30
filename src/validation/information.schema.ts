b82c29089476a5060c6c288760424e38faff89db
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-08-30 10:40:38 +0330

    add regex persian

--- src/validation/information.schema.ts
+++ src/validation/information.schema.ts
@@ -1,15 +1,23 @@
 import { z } from "zod";
 
+const persianRegex = /^[\u0600-\u06FF\s]+$/;
+
 export const InformationSchema = z.object({
   firstName: z
     .string({ required_error: "نام الزامی است" })
+    .regex(persianRegex, "فقط حروف فارسی مجاز است")
     .min(2, "نام باید حداقل 2 کاراکتر باشد"),
   lastName: z
     .string({ required_error: "نام خانوادگی الزامی است" })
+    .regex(persianRegex, "فقط حروف فارسی مجاز است")
     .min(2, "نام خانوادگی باید حداقل 2 کاراکتر باشد"),
   maeitalStatus: z.enum(["single", "married", "divorces"]),
   genderL: z.enum(["male", "female"]),
-  fatherName: z.string().min(2, "نام پدر باید حداقل 2 کاراکتر باشد").optional(),
+  fatherName: z
+    .string()
+    .min(2, "نام پدر باید حداقل 2 کاراکتر باشد")
+    .regex(persianRegex, "فقط حروف فارسی مجاز است")
+    .optional(),
 });
 
 export type informationFormValue = z.infer<typeof InformationSchema>;
