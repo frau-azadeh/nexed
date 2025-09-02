231ab42691b02eb59b7e47a9bd91927f0b1951be
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-02 12:05:14 +0330

    update prettier

--- src/app/wizard/page.tsx
+++ src/app/wizard/page.tsx
@@ -5,5 +5,5 @@ export default function Wizard() {
     <div>
       <StepOne />
     </div>
-  )
+  );
 }
--- src/validation/cinema.steptwo.schema.ts
+++ src/validation/cinema.steptwo.schema.ts
@@ -1,31 +1,29 @@
 import { z } from "zod";
 
-const toInt = (val:unknown)=> val === "" || val == null ? undefined : Number(String(val))
+const toInt = (val: unknown) =>
+  val === "" || val == null ? undefined : Number(String(val));
 
 export const StepTwoSchema = z.object({
-    movie: z
-    .enum(["dune2", "oppenheimer", "insideout2"],{
-        required_error: "فیلم را انتخاب کنید"
-    }),
+  movie: z.enum(["dune2", "oppenheimer", "insideout2"], {
+    required_error: "فیلم را انتخاب کنید",
+  }),
 
-    showtime: z
-    .enum(["18:00", "20:00", "22:00"],{
-        required_error: "سانس را انتخاب کنید"
-    }),
+  showtime: z.enum(["18:00", "20:00", "22:00"], {
+    required_error: "سانس را انتخاب کنید",
+  }),
 
-    seatType: z
-    .enum(["normal", "vip"],{
-        required_error: "نوع صندلی را انتخاب کنید"
-    }),
+  seatType: z.enum(["normal", "vip"], {
+    required_error: "نوع صندلی را انتخاب کنید",
+  }),
 
-    quantity: z.preprocess(
-        toInt,
-        z
-        .number({required_error: "تعداد بلیط را وارد کنید"})
-        .int("عدد صحیح باشد")
-        .gte(1,"حداقل یک عدد")
-        .lte(6,"حداکثر شش عدد"),
-    ),
-})
+  quantity: z.preprocess(
+    toInt,
+    z
+      .number({ required_error: "تعداد بلیط را وارد کنید" })
+      .int("عدد صحیح باشد")
+      .gte(1, "حداقل یک عدد")
+      .lte(6, "حداکثر شش عدد"),
+  ),
+});
 
-export type StepTwoFormValue = z.infer<typeof StepTwoSchema>
+export type StepTwoFormValue = z.infer<typeof StepTwoSchema>;
