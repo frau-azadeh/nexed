442f78c3622cc2892e9dd80eeab160ab75464ca4
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-03 13:53:36 +0330

    update prettier

--- src/app/wizard2/page.tsx
+++ src/app/wizard2/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Step1FormValue } from "@/validation/wizard.step1.schema";
 import { useState } from "react";
@@ -6,11 +6,10 @@ import StepOne from "../components/form/wizard2/StepOne";
 import { Step2FormValue } from "@/validation/wizard.step2.schema";
 import StepTwo from "../components/form/wizard2/StepTwo";
 
-
 export default function Wizard2() {
   const [step, setStep] = useState<0 | 1 | 2 | 3>(0);
   const [s1, setS1] = useState<Step1FormValue | null>(null);
-  const[s2, setS2] = useState<Step2FormValue | null>(null)
+  const [s2, setS2] = useState<Step2FormValue | null>(null);
   return (
     <main className="p-6">
       {step === 0 && (
@@ -21,8 +20,8 @@ export default function Wizard2() {
           }}
         />
       )}
-      {step === 1&&(
-         <StepTwo
+      {step === 1 && (
+        <StepTwo
           // ⬇️ فقط اگر s2 موجود است defaultValues را پاس بده
           {...(s2 ? { defaultValues: s2 } : {})}
           onBack={() => setStep(0)}
