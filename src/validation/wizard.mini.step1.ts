989c57f6454b869f9fce61e5e7239a045c600031
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-04 21:00:59 +0330

    update prettier

--- src/validation/wizard.mini.step1.ts
+++ src/validation/wizard.mini.step1.ts
@@ -1,9 +1,9 @@
 import { boolean, z } from "zod";
 
 export const MiniStep1Schema = z.object({
-    accept: z
+  accept: z
     .boolean()
-    .refine((val)=>val === true, {message: "پذیرش قوانین الزامی است"}),
-})
+    .refine((val) => val === true, { message: "پذیرش قوانین الزامی است" }),
+});
 
-export type MiniStep1FormValue = z.infer<typeof MiniStep1Schema>
+export type MiniStep1FormValue = z.infer<typeof MiniStep1Schema>;
