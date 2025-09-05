46f45d076496493f0fe46d33dff1f20aa51e3ae7
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-05 18:17:45 +0330

    update prettier

--- src/validation/wizard-mini.final.ts
+++ src/validation/wizard-mini.final.ts
@@ -1,11 +1,9 @@
 import { z } from "zod";
 
 export const MiniFinalSchema = z.object({
-  confirm: z
-    .boolean()
-    .refine((val) => val === true, {
-      message: "برای ارسال نهایی باید تایید شود",
-    }),
+  confirm: z.boolean().refine((val) => val === true, {
+    message: "برای ارسال نهایی باید تایید شود",
+  }),
 });
 
 export type MIniFinalFormValue = z.infer<typeof MiniFinalSchema>;
